d01e179c3047bb73a6bda5de7ab62f03409713bc
Matheus Antonio <matheusantonio232@gmail.com>
2020-10-17 12:33:21 -0300

    css/pages/libs added

---
+++ Web/Larn/src/main/webapp/js/main.js
@@ -0,0 +1,16 @@
+$(document).ready(function() {
+ 
+    $("#owl-demo").owlCarousel({
+   
+        navigation : true, // Show next and prev buttons
+        slideSpeed : 300,
+        paginationSpeed : 400,
+        singleItem:true,
+        items : 1,
+        loop: true
+     
+    });
+   
+  });
+
+
