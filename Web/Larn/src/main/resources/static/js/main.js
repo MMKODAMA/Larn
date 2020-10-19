c8c1295490499e0f3c0d3bd13237f8c44f0835b1
Matheus Antonio <matheusantonio232@gmail.com>
2020-10-19 19:19:24 -0300

    Att front end paginas login, cadastros e perfil

--- Web/Larn/src/main/resources/static/js/main.js
+++ Web/Larn/src/main/resources/static/js/main.js
@@ -2,15 +2,23 @@ $(document).ready(function() {
  
     $("#owl-demo").owlCarousel({
    
-        navigation : true, // Show next and prev buttons
+      navigation : true, // Show next and prev buttons
         slideSpeed : 300,
         paginationSpeed : 400,
-        singleItem:true,
-        items : 1,
-        loop: true
+         singleItem:true,
+       items : 1,
+         loop: true
      
     });
    
+ });
+
+
+
+$( "#eye" ).mousedown(function() {
+    $("#inputPassword").attr("type", "text");
   });
-
-
+  
+  $( "#eye" ).mouseup(function() {
+    $("#inputPassword").attr("type", "password");
+  });
