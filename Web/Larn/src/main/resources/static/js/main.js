8b2235942512a8127f95a7d8029cb8bb44c2605b
Matheus Antonio <matheusantonio232@gmail.com>
2020-10-22 19:15:31 -0300

    Criação página 404, atualziação de css e js

--- Web/Larn/src/main/resources/static/js/main.js
+++ Web/Larn/src/main/resources/static/js/main.js
@@ -1,24 +1,51 @@
-$(document).ready(function() {
- 
-    $("#owl-demo").owlCarousel({
-   
-      navigation : true, // Show next and prev buttons
-        slideSpeed : 300,
-        paginationSpeed : 400,
-         singleItem:true,
-       items : 1,
-         loop: true
-     
-    });
-   
- });
+$(document).ready(function () {
 
+  $("#owl-demo").owlCarousel({
 
+    navigation: true, // Show next and prev buttons
+    slideSpeed: 300,
+    paginationSpeed: 400,
+    singleItem: true,
+    items: 1,
+    loop: true
 
-$( "#eye" ).mousedown(function() {
-    $("#inputPassword").attr("type", "text");
   });
-  
-  $( "#eye" ).mouseup(function() {
-    $("#inputPassword").attr("type", "password");
-  });
+
+});
+
+
+$("#eye").mousedown(function () {
+  $("#inputPassword").attr("type", "text");
+});
+
+$("#eye").mouseup(function () {
+  $("#inputPassword").attr("type", "password");
+});
+
+
+////Effects 404 page
+const title = document.querySelector('.error_title')
+//////// Light //////////
+document.onmousemove = function (e) {
+  let x = e.pageX - window.innerWidth / 2;
+  let y = e.pageY - window.innerHeight / 2;
+
+  title.style.setProperty('--x', x + 'px');
+  title.style.setProperty('--y', y + 'px');
+}
+
+////////////// Shadow ///////////////////
+title.onmousemove = function (e) {
+  let x = e.pageX - window.innerWidth / 2;
+  let y = e.pageY - window.innerHeight / 2;
+
+  let rad = Math.atan2(y, x).toFixed(2);
+  let length = Math.round(Math.sqrt((Math.pow(x, 2)) + (Math.pow(y, 2))) / 10);
+
+  let x_shadow = Math.round(length * Math.cos(rad));
+  let y_shadow = Math.round(length * Math.sin(rad));
+
+  title.style.setProperty('--x-shadow', -x_shadow + 'px');
+  title.style.setProperty('--y-shadow', -y_shadow + 'px');
+
+}
