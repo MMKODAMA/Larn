ab634990bcde86f5df46524329f3f84c45b570e9
Matheus Antonio <matheusantonio232@gmail.com>
2020-11-07 01:29:15 -0300

    Feature de professor listar aulas criado

--- Web/Larn/src/main/java/com/example/larn/repository/AulaRepository.java
+++ Web/Larn/src/main/java/com/example/larn/repository/AulaRepository.java
@@ -4,8 +4,11 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.example.larn.model.Aula;
+import com.example.larn.model.Teacher;
 
 @Repository
 public interface AulaRepository extends JpaRepository<Aula, Integer> {
+	
+	Iterable<Aula> findByTeacher(Teacher teacher);
 
 }
